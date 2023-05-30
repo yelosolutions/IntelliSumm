87802abe32c6e7cd192e11e4607b9c572914efbc
jay <ronokamau@gmail.com>
2023-05-31 01:35:25 +0300

    getting started

---
+++ summarizer-v1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
