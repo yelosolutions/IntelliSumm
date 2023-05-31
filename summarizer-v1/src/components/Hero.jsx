ede01c58d973460c8fa4f80008080499b73ac809
jay <ronokamau@gmail.com>
2023-06-01 01:08:04 +0300

    Hero section update

--- summarizer-v1/src/components/Hero.jsx
+++ summarizer-v1/src/components/Hero.jsx
@@ -5,9 +5,13 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="intellisumm logo" className='w-28 object-contain'/>
-      <button className="p-3 bg-black col-auto">Github</button>
+      <button type='button' onClick={() => window.open('https://github.com/yelosolutions')} className='black_btn'>Github</button>
       </nav>
-      <h1></h1>
+      <h1 className='head_text' >Article Summarizer using 
+      <br className='max-md:hidden'/>
+      <span className='orange_gradient'> OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'> Enhance your productivity with IntelliSumm, an article summarizer that transform long articles into clear and coincise summaries.</h2>
     </header>
   )
 }
