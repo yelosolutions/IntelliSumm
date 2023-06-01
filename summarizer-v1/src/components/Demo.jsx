a30bc3660bab8a09be2d473c1a0a6c2c784815df
jay <ronokamau@gmail.com>
2023-06-01 23:37:52 +0300

    Demo implementation

--- summarizer-v1/src/components/Demo.jsx
+++ summarizer-v1/src/components/Demo.jsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react'
+
+import { copy, linkIcon, loader, tick } from '../assets';
+
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      {}
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center" action="" onSubmit={() => {}}>
+          <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
+          <input type="url" 
+          placeholder="Enter url"
+          />
+        </form>
+      </div>
+    </section>
   )
 }
 
